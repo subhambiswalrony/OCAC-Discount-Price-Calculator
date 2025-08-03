6c4d25aa44ad0f73dd68349cdd3748cb68212672
Subham Biswal <biswalsubhamrony@gmail.com>
2025-08-04 01:06:32 +0530

    new Project

---
+++ src/discountpricecalculator/DiscountPriceCalculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+
+/**
+ *
+ * @author Subham
+ */
+
+package discountpricecalculator;
+
+public class DiscountPriceCalculator {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new WelcomePage().setVisible(true));
+    }
+}
